5dd957794f0c7c01d18d9f186de85c2095f9e08f
Nischal Prasai <narahariprasai50@gmail.com>
2026-02-21 07:13:23 +0545

    String methods Learned

---
+++ MyApp/Day3.cs
@@ -0,0 +1,66 @@
+using System;
+
+public class Day3
+{
+    public void Third()
+    {
+        //---------------------STRING METHODS---------------------//
+        /*
+        //Length of string
+        string name = "BaburaoGaitonde";
+        int length = name.Length;
+        Console.WriteLine(length);
+
+        //ToUpper and ToLower
+        name = name.ToUpper();
+        name = name.ToLower();
+        System.Console.WriteLine(name);
+        
+
+        //Contains
+        string name = "BaburaoGaitonde";
+        Console.WriteLine(name.Contains('z'));
+        bool contains = name.Contains("rao");
+        Console.WriteLine(contains);
+
+        //Substring
+        string name = "BaburaoGaitonde";
+        string slicedname = name.Substring(0,7); // 0->starting index, 7->Length
+        Console.WriteLine(slicedname);
+
+        //Replace
+        string name = "BaburaoGaitonde";
+        string newname = name.Replace("Gaitonde", "Aapte");
+        Console.WriteLine(newname);
+
+        //Trim
+        string name = "Baburao Aapte";
+        Console.WriteLine(name.Trim());
+
+        //IndexOf
+        string name = "Baburao Ganpatrao Aapte";
+        Console.WriteLine(name.IndexOf('o'));
+
+        //StartsWith and EndsWith
+        string name = "Baburao Ganpatrao Aapte";
+        Console.WriteLine(name.ToLower().StartsWith('b'));
+        Console.WriteLine(name.EndsWith("Aapte"));
+        
+
+        //Split
+        string sentence = "My name is.Sheela";
+        string[] words = sentence.Split(' ', '.');
+        foreach ( string word in words)
+        {
+            Console.WriteLine(word);
+        }
+
+        //EqualsTo
+        string a = "Babu";
+        string b = "babu";
+        System.Console.WriteLine(a.Equals(b));
+        System.Console.WriteLine(a.Equals(b, StringComparison.OrdinalIgnoreCase));
+        */
+
+    }
+}
